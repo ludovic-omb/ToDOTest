80d4c63b39d303ad0675db49013b78a2cc8f38fe
Ludovic O <ludovicomombo@icloud.com>
2024-10-01 14:55:58 +0200

    Initial Commit

---
+++ ToDOTest/ToDOTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOTestApp.swift
+//  ToDOTest
+//
+//  Created by Ludovic Omombo on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDOTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDOTestTests/ToDOTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOTestTests.swift
+//  ToDOTestTests
+//
+//  Created by Ludovic Omombo on 01/10/2024.
+//
+
+import Testing
+@testable import ToDOTest
+
+struct ToDOTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
